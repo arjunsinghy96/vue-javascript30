9bddb563a424a56002b1def80d005bc06cf4f1eb
Arjun Singh Yadav <arjunsinghy96@gmail.com>
2018-12-15 12:35:44 +0530

    Add Github link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="flex flex-1 items-center justify-center h-screen bg-grey-lighter">
+<div class="flex flex-col h-screen">
+  <div class="w-8 h-8 border border-black hover:bg-black rounded-full flex flex-1 items-center justify-center fixed pin-t pin-r m-4">
+    <a class="h-full w-full no-underline text-black hover:text-white text-xl items-center flex flex-1 items-center justify-center" href="https://github.com/arjunsinghy96/vue-javascript30.git">
+      <i class="fab fa-github-alt"></i>
+    </a>
+  </div>
+  <div class="flex flex-col h-48 items-center justify-center mx-4">
+    <p class="text-4xl md:text-5xl underline">Vue JavaScript30</p>
+    <p class="pt-3">This website contains the JavaScript30 examples but with Vue.js and a little personal touch.</p>
+  </div>
+  <div class="flex flex-1 items-center justify-center h-full bg-grey-lighter">
     <div class="flex flex-wrap justify-center">
       <router-link class="no-underline btn" to="/day1">01. Drum Kit</router-link>
       <router-link class="no-underline btn" to="/day2">02. Clock</router-link>
@@ -7,6 +17,7 @@
       <router-link class="no-underline btn" to="/day5">05. Flex Gallery</router-link>
     </div>
   </div>
+</div>
 </template>
 
 <script>
